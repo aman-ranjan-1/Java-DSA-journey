2a199d3bb3ef2e1f111310bd802aa09053858d05
aman-ranjan-1 <aman.ranjan21a@gmail.com>
2026-02-28 13:50:50 +0530

    Updated 1st java practice file

--- 01.Basics/HelloWorld.java
+++ 01.Basics/HelloWorld.java
@@ -1,6 +1,6 @@
 class HelloWorld {
     public static void main(String args[]){
         System.out.println("Hello World");
-        System.out.println("I am Aman. Startitng My JAVA+DSA journey.");
+        System.out.println("I am Aman. Starting My JAVA+DSA journey.");
     }
 }
