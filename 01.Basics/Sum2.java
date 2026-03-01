b224ab9e0de584dc7e8b252dff07336d63b19003
aman-ranjan-1 <aman.ranjan21a@gmail.com>
2026-03-01 13:58:57 +0530

    Added a Practice code to get Sum

---
+++ 01.Basics/Sum2.java
@@ -0,0 +1,16 @@
+//Problem : Add two number (use of scanner class)
+
+import java.util.*;
+
+public class Sum2 {
+    public static void main(String[] args) {
+        int a,b,sum=0;
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter num1:");
+        a = sc.nextInt();
+        System.out.println("Enter num2:");
+        b = sc.nextInt();
+        sum = a+b;
+        System.out.println("Added number is : "+sum);
+    }
+}
