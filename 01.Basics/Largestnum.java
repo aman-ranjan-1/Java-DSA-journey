f266bcf226758f50d71c6e5e87b7b79a9104b6ed
aman-ranjan-1 <aman.ranjan21a@gmail.com>
2026-03-01 23:07:04 +0530

    Added practice codes

---
+++ 01.Basics/Largestnum.java
@@ -0,0 +1,33 @@
+// Problem : Find the largest number from the given 3 numbers
+
+import java.util.*;
+public class Largestnum {
+    public static void main(String[] args) {
+        int a ,b,c;
+        Scanner ac = new Scanner (System.in);
+        System.out.println("Enter Three numbers : ");
+        a = ac.nextInt();
+        b = ac.nextInt();
+        c = ac.nextInt();
+        // int max = a;
+        // if (b > max) {
+        //     max = b;
+        // }
+        // if(c > max){
+        //     max = c;
+        // }
+        // System.out.println("Largest number is "+max);
+        // int max;
+        // if (a>b && a>c){
+        //     max = a;
+        // }
+        // else if(b > c && b > a){
+        //     max = b;
+        // }
+        // else{
+        //     max = c;
+        // }
+        int max = Math.max(c,Math.max(a,b));
+        System.out.println("Largest number is : " +max);
+    }
+}
---
+++ 02.Patterns/pattern1.java
@@ -0,0 +1,19 @@
+// Problem : Print this pattern (use of loop)
+// * * * * *
+// * * * * *
+// * * * * *
+// * * * * *
+// * * * * *
+
+
+public class pattern1 {
+    public static void main(String[] args) {
+        int i,j;
+        for (i=0;i<5;i++){
+            for (j=0;j<5;j++){
+                System.out.print("* ");
+            }
+            System.out.println(" ");
+        }
+    }
+}
