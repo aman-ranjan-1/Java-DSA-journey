1ed93c8e938fc2e31e862fbc5b84b60fbd17d909
aman-ranjan-1 <aman.ranjan21a@gmail.com>
2026-03-01 23:46:44 +0530

    Updated Area1.java file

--- 01.Basics/Area1.java
+++ 01.Basics/Area1.java
@@ -4,11 +4,12 @@ import java.util.*;
 
 public class Area1 {
     public static void main(String[] args) {
+        float rad,area;
         Scanner ac = new Scanner (System.in);
-        float radius , Area;
-        radius = ac.nextFloat();
-        Area = 3.14f * radius * radius ;
-        System.out.println("Area of the circle is "+Area);
+        System.out.println("Enter radius : ");
+        rad = ac.nextFloat();
+        area = 3.14f * rad * rad ;
+        System.out.println("Area of the circle is "+area);
         ac.close();
     }
 }
