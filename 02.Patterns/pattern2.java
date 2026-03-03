ed4cd128c243f6aa3bff6dd0657fbb11ca77a016
aman-ranjan-1 <aman.ranjan21a@gmail.com>
2026-03-03 21:16:47 +0530

    added a basic problem code

---
+++ 02.Patterns/pattern2.java
@@ -0,0 +1,22 @@
+// Problem : Print this pattern :
+
+// *
+// * *
+// * * *
+// * * * *
+// * * * * *
+import java.util.*;
+public class pattern2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value of n : ");
+        int n = sc.nextInt();
+        System.out.println();
+        for (int row = 1;row <= n ; row++){
+            for (int col = 1; col <= row; col++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
