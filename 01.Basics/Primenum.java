66529f4034122a2011fd7757371e696943ba2518
aman-ranjan-1 <aman.ranjan21a@gmail.com>
2026-03-03 18:36:07 +0530

    Added Primenum.java file

---
+++ 01.Basics/Primenum.java
@@ -0,0 +1,26 @@
+// Check whether the given number is prime or not 
+
+import java.util.Scanner;
+
+public class Primenum {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter a Num : ");
+        int n = in.nextInt();
+        boolean ans = isPrime(n);
+        System.out.println(ans);
+    }
+    static boolean isPrime(int n){
+        if (n <= 1){
+            return false;
+        }
+        int c = 2;
+        while(c <= Math.sqrt(n)){
+            if(n%c == 0){
+                return false;
+            }
+            c++;
+        }
+        return true;
+    }
+}
