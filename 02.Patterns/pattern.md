dbd893e1a1d76007f54ee566e8fe885b5d9ac8a5
aman-ranjan-1 <aman.ranjan21a@gmail.com>
2026-03-07 22:08:04 +0530

    Added pattern and recursion problems

--- 02.Patterns/pattern.md
+++ 02.Patterns/pattern.md
@@ -2,7 +2,7 @@ All the pattern Problems solved -
 
 File Details - Pattern 1 - pattern1.java
 Pattern 2 - pattern2.java
-Pattern (3 - 7) - patterns.java
+Pattern (3 - 12) - patterns.java
 
 Pattern Problems --- 
 Pattern 1 - 
@@ -57,3 +57,57 @@ o o o o o
   o o o
    o o
     o
+
+
+Pattern 8 -
+
+5 4 3 2 1
+5 4 3 2
+5 4 3
+5 4
+5
+
+Pattern 9 -
+
+1 2 3 4 5 
+1 2 3 4 
+1 2 3 
+1 2
+1 
+
+Pattern 10 -
+
+0 2 3 4 5 
+1 0 3 4 5
+1 2 0 4 5
+1 2 3 0 5
+1 2 3 4 0
+
+Pattern 11 - 
+
+1 
+0 1 
+1 0 1
+0 1 0 1
+1 0 1 0 1
+
+Pattern 12 - 
+
+1 2 3 4 5 5 4 3 2 1 
+1 2 3 4     4 3 2 1
+1 2 3         3 2 1
+1 2             2 1
+1                 1
+1 2             2 1
+1 2 3         3 2 1
+1 2 3 4     4 3 2 1
+1 2 3 4 5 5 4 3 2 1
+
+
+Pattern 13 - 
+
+ 1 1 1 1 1 
+ 2 2 2 2 
+ 3 3 3 
+ 4 4
+ 5 
--- 02.Patterns/patterns.java
+++ 02.Patterns/patterns.java
@@ -5,7 +5,7 @@
 
 public class patterns {
     public static void main(String[] args) {
-        basicpattern(5);         // call any pattern :)
+        pattern12(5);         // call any pattern :)
     }
     // basic pattern - * * * * *
 
@@ -108,5 +108,145 @@ public class patterns {
             System.out.println();
         }
     }
+    // pattern 8 - 
+    
+    // 5 4 3 2 1
+    // 5 4 3 2
+    // 5 4 3
+    // 5 4
+    // 5
 
+    public static void pattern8(int n){
+        for (int row = 1;row<=n;row++){
+            for (int col = n; col >= row; col--) {
+                System.out.print(" "+col);
+            }
+            System.out.println();
+        }
+    }
+
+    // Pattern 9 -
+    // 1 2 3 4 5 
+    // 1 2 3 4 
+    // 1 2 3 
+    // 1 2
+    // 1 
+
+    public static void pattern9(int n){
+        for (int row = 1;row<=n;row++){
+            for (int col = n; col >= row; col--) {
+                System.out.print(" "+(n-col+1));
+            }
+            System.out.println();
+        }
+    }
+
+    // Pattern 10 -
+
+    // 0 2 3 4 5 
+    // 1 0 3 4 5
+    // 1 2 0 4 5
+    // 1 2 3 0 5
+    // 1 2 3 4 0
+
+    public static void pattern10(int n){
+        for (int i=1;i<=n;i++){
+            for (int j=1;j<=n;j++){
+                if(i==j){
+                    System.out.print(0+" ");
+                }
+                else{
+                    System.out.print(j+" ");
+                }
+            }
+            System.out.println(" ");
+        }
+    }
+
+    //     Pattern 11 - 
+
+    // 1 
+    // 0 1 
+    // 1 0 1
+    // 0 1 0 1
+    // 1 0 1 0 1
+
+    public static void pattern11(int n){
+        for (int row = 1 ; row <= n; row++){
+            for(int col = 1 ; col <= row ;col++){
+                if((row+col)%2==0){
+                    System.out.print(1+" ");
+                }
+                else{
+                    System.out.print(0+" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    // Pattern 12 - 
+
+    // 1 2 3 4 5 5 4 3 2 1 
+    // 1 2 3 4     4 3 2 1
+    // 1 2 3         3 2 1
+    // 1 2             2 1
+    // 1                 1
+    // 1                 1
+    // 1 2             2 1
+    // 1 2 3         3 2 1
+    // 1 2 3 4     4 3 2 1
+    // 1 2 3 4 5 5 4 3 2 1
+
+    public static void pattern12(int n){
+        //upper half
+        for (int i = n ; i >= 1 ; i--){
+            //left numbers 
+            for (int j = 1 ; j <= i ; j++){
+                System.out.print(j+ " ");
+            }
+            //space
+            for (int s = 1 ; s <= 2*(n-i);s++){
+                System.out.print("  ");
+            }
+            //right numbers
+            for (int j = i; j >= 1; j--) {
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+        //lowerhalf
+        for (int i = 1 ; i <= n ; i++){
+            //left numbers 
+            for (int j = 1 ; j <= i ; j++){
+                System.out.print(j+ " ");
+            }
+            //space
+            for (int s = 1 ; s <= 2*(n-i);s++){
+                System.out.print("  ");
+            }
+            //right numbers
+            for (int j = i; j >= 1; j--) {
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+
+    // Pattern 13 - 
+
+    // 1 1 1 1 1 
+    // 2 2 2 2 
+    // 3 3 3 
+    // 4 4
+    // 5 
+
+    public static void pattern13(int n){
+        for (int row = 1; row <= n; row++) {
+            for (int col = n; col >= row; col--) {
+                System.out.print(" "+row);
+            }
+            System.out.println();
+        }
+    }
 }
